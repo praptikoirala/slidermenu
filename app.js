b70a807b0acf0dda87534407685a305c9519c88e
prapti koirala <praptikoirala6@gmail.com>
2021-05-05 16:12:37 +0545

    pop-up box added

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const toggle = document.querySelector('.toggle');
 const navigate = document.querySelector('.navbar');
+const lines = document.querySelectorAll('.line');
+const signin = document.querySelector('.signup-btn');
+const dispbox = document.querySelector('.pop-up');
+const close = document.querySelector('.close-cross');
 
-toggle.addEventListener('click' , printOut);
+toggle.addEventListener('click' , navigationShow);
 
-function printOut(e){
+function navigationShow(e){
 
   if(navigate.classList.contains("show-nav")){
     navigate.classList.remove("show-nav");
   }else{
     navigate.classList.add("show-nav");
   }
+
+  for(i = 0; i < lines.length; i++){
+    if(lines[i].classList.contains("rotate")){
+      lines[i].classList.remove("rotate");
+    }else{
+      lines[i].classList.add("rotate");
+    }
+  }
 }
+
+signin.addEventListener('click' , popupShow);
+
+function popupShow(e){
+  dispbox.style.display = 'block';
+}
+
+close.addEventListener('click' , popupHide);
+
+function popupHide(e){
+  dispbox.style.display = 'none';
+}
+
+
